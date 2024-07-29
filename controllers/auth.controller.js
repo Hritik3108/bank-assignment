bf27f03b3197177999e503e17129018e09108b8a
Hritik3108 <hritiksn009@gmail.com>
2024-07-29 08:25:45 +0530

    updated auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -28,6 +28,7 @@ exports.login = async (req, res) => {
 };
 
 exports.registration = async (req, res) => {
+    console.log('registration init')
     const { username,email,password,role } = req.body;
     try {
         let newUser;
@@ -35,15 +36,20 @@ exports.registration = async (req, res) => {
             newUser = await User.create({pp:req.file.filename,username,email, password,role});
           }else{
             newUser = await User.create({username,email,password,role});
-          }
+          }          
+        console.log('new user created',newUser);
+
         if (newUser) {
             const token = jwt.sign({userId: newUser._id, role: newUser.role },process.env.JWT_SECRET,{ expiresIn: process.env.JWT_EXPIRES_IN });
             newUser.password=undefined;
+            console.log('new user',newUser);
             res.status(200).json({ user:newUser,token });
         } else {
+            console.log('error');
             res.status(400).json({ error: "User not saved" });
         }  
     } catch (error) {
+        console.log('catch error');
         res.status(500).json({ message: error.message });
     }
 };
